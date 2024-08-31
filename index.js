e0d05c6c6ce0aff04151d945650d13916fe169a7
Panagiotis Bolimos <115077668+pbolymos@users.noreply.github.com>
2024-08-31 16:33:51 +0000

    add-color-2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function info() {
   return {
     apiversion: "1",
     author: "", // TODO: Your Battlesnake Username
-    color: "#888888", // TODO: Choose color
+    color: "#444444", // TODO: Choose color
     head: "default", // TODO: Choose head
     tail: "default", // TODO: Choose tail
   };
